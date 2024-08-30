7919073a3c0aad1038050b8f808a7a7eba09891f
yilmaz1998 <snn.ylmz9804@gmail.com>
2024-08-30 00:32:54 -0400

    authentication has been added for reviews

--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const reviewController = require('../controllers/reviewController')
+const authenticate = require('../middleware/authenticate')
 
-router.post('/new', reviewController.createReview)
+router.post('/new', authenticate, reviewController.createReview)
 router.get('/:wineId', reviewController.getReviewByWine)
-router.get('/:id/get', reviewController.getReviewById)
-router.put('/:id', reviewController.updateReview)
-router.delete('/:id', reviewController.deleteReview)
+router.get('/:id/get', authenticate, reviewController.getReviewById)
+router.put('/:id', authenticate,reviewController.updateReview)
+router.delete('/:id', authenticate, reviewController.deleteReview)
 
 module.exports = router
